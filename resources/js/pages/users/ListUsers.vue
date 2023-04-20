516ffd59d940b66ddfa14f233cf46308e408ebb7
sargsyan.shahen <sargsyan.shahen@gmail.com>
2023-04-20 21:11:34 +0400

    Client Side Form Validation using Vue, VeeValidate and Yup | Part 7

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,5 +1,8 @@
 <script setup>
-import {ref, onMounted, reactive} from "vue";
+import axios from "axios";
+import { ref, onMounted, reactive } from "vue";
+import { Form, Field } from 'vee-validate';
+import * as yup from 'yup';
 
 let users = ref([]);
 
@@ -9,20 +12,20 @@ const getUsers =() => {
             users.value = response.data;
     })
 }
-const form = reactive({
-    name: '',
-    email: '',
-    password: '',
+const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(8),
 });
 
-const createUser = () => {
-    axios.post('/api/users', form)
+const createUser = (values, { resetForm }) => {
+    axios.post('/api/users', values)
         .then((response) => {
             users.value.unshift(response.data)
-            form.name = '';
-            form.email = '';
-            form.password = '';
+            getUsers();
+
             $('#createUserModal').modal('hide');
+            resetForm();
         });
 }
 
@@ -95,28 +98,35 @@ onMounted(() => {
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <form autocomplete="off">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input v-model="form.name" type="text" class="form-control" id="name" aria-describedby="" placeholder="Enter full name">
-                        </div>
+                <Form @submit="createUser" :validation-schema="schema" v-slot="{ errors }">
+                    <div class="modal-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <Field name="name" type="text" class="form-control" :class="{'is-invalid': errors.name}" id="name"
+                                       aria-describedby="nameHelp" placeholder="Enter full name" />
+                                <span class="invalid-feedback">{{ errors.name }}</span>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="name">Email</label>
-                            <input v-model="form.email" type="text" class="form-control" id="email" aria-describedby="" placeholder="Enter email">
-                        </div>
+                            <div class="form-group">
+                                <label for="name">Email</label>
+                                <Field name="email" type="email" class="form-control" :class="{'is-invalid': errors.email}" id="email"
+                                       aria-describedby="nameHelp" placeholder="Enter email" />
+                                <span class="invalid-feedback">{{ errors.email }}</span>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="name">Password</label>
-                            <input v-model="form.password" type="text" class="form-control" id="name" aria-describedby="" placeholder="Enter password">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button @click="createUser" type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                            <div class="form-group">
+                                <label for="name">Password</label>
+                                <Field name="password" type="password" class="form-control" :class="{'is-invalid': errors.password}" id="password"
+                                       aria-describedby="nameHelp" placeholder="Enter password" />
+                                <span class="invalid-feedback">{{ errors.password }}</span>
+                            </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </Form>
             </div>
         </div>
     </div>
