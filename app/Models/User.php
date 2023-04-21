2058ae4d984cc159a2cc65aaccc0967fd120d255
sargsyan.shahen <sargsyan.shahen@gmail.com>
2023-04-21 17:40:53 +0400

    How to use PHP 8.1 Enums for creating role type using Laravel and Vue 3 | Part 14

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\RoleType;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -49,5 +51,13 @@ class User extends Authenticatable
     public function getFormattedCreatedAtAttribute()
     {
         return $this->created_at->format(config('app.date_format'));
+    }
+
+    public function role(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => RoleType::from($value)->name,
+        );
+
     }
 }
